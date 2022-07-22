9e2b8d8ab886567b1da342d194fb1b43ff6a5e62
rike <hello@rike.dev>
2022-07-22 22:32:20 +0200

    a href to Link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Mint from "./pages/Mint";
 import Members from "./pages/Members";
@@ -39,87 +39,87 @@ function App() {
   }, [contract, address]);
 
   return (
-    <div className="terminal">
-      <div className="container">
-        <div className="terminal-nav">
-          <header className="terminal-logo">
-            <div className="logo terminal-prompt">
-              <a href="/" className="no-style">
-                Haru's Hut
-              </a>
-            </div>
-          </header>
-          <nav className="terminal-menu">
-            <ul vocab="https://schema.org/" typeof="BreadcrumbList">
-              <li>
-                <a href="/" className="menu-item active">
-                  <span>Home</span>
-                </a>
-              </li>
-              {isConnected && !isMember ? (
+    <BrowserRouter>
+      <div className="terminal">
+        <div className="container">
+          <div className="terminal-nav">
+            <header className="terminal-logo">
+              <div className="logo terminal-prompt">
+                <Link to="/" className="no-style">
+                  Haru's Hut
+                </Link>
+              </div>
+            </header>
+            <nav className="terminal-menu">
+              <ul vocab="https://schema.org/" typeof="BreadcrumbList">
                 <li>
-                  <a
-                    href="/mint"
+                  <a href="/" className="menu-item active">
+                    <span>Home</span>
+                  </a>
+                </li>
+                {isConnected && !isMember ? (
+                  <li>
+                    <Link
+                      to="/mint"
+                      property="item"
+                      typeof="WebPage"
+                      className="menu-item"
+                    >
+                      <span property="name">Mint membership</span>
+                    </Link>
+                  </li>
+                ) : null}
+                <li>
+                  <Link
+                    to="/info"
                     property="item"
                     typeof="WebPage"
                     className="menu-item"
                   >
-                    <span property="name">Mint membership</span>
-                  </a>
+                    <span property="name">More info</span>
+                  </Link>
                 </li>
-              ) : null}
-              <li>
-                <a
-                  href="/info"
-                  property="item"
-                  typeof="WebPage"
-                  className="menu-item"
-                >
-                  <span property="name">More info</span>
-                </a>
-              </li>
-              {isConnected && isMember ? (
-                <>
-                  <li property="itemListElement" typeof="ListItem">
-                    <a
-                      href="/members"
-                      property="item"
-                      typeof="WebPage"
-                      className="menu-item"
-                    >
-                      <span property="name">Members</span>
-                    </a>
-                  </li>
-                  <li property="itemListElement" typeof="ListItem">
-                    <a
-                      href="/proposals"
-                      property="item"
-                      typeof="WebPage"
-                      className="menu-item"
-                    >
-                      <span property="propsals">Proposals</span>
-                    </a>{" "}
-                  </li>
-                  <li property="itemListElement" typeof="ListItem">
-                    <a
-                      href="/haru"
-                      property="item"
-                      typeof="WebPage"
-                      className="menu-item"
-                    >
-                      <span property="name">Gimme $HARU</span>
-                    </a>
-                  </li>
-                </>
-              ) : null}
-              <li property="itemListElement" typeof="ListItem">
-                <ConnectButton />
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div>
-          <BrowserRouter>
+                {isConnected && isMember ? (
+                  <>
+                    <li property="itemListElement" typeof="ListItem">
+                      <Link
+                        to="/members"
+                        property="item"
+                        typeof="WebPage"
+                        className="menu-item"
+                      >
+                        <span property="name">Members</span>
+                      </Link>
+                    </li>
+                    <li property="itemListElement" typeof="ListItem">
+                      <Link
+                        to="/proposals"
+                        property="item"
+                        typeof="WebPage"
+                        className="menu-item"
+                      >
+                        <span property="propsals">Proposals</span>
+                      </Link>{" "}
+                    </li>
+                    <li property="itemListElement" typeof="ListItem">
+                      <Link
+                        to="/haru"
+                        property="item"
+                        typeof="WebPage"
+                        className="menu-item"
+                      >
+                        <span property="name">Gimme $HARU</span>
+                      </Link>
+                    </li>
+                  </>
+                ) : null}
+                <li property="itemListElement" typeof="ListItem">
+                  <ConnectButton />
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div>
             <Routes>
               <Route path="/" element={<Home isMember={isMember} />} />
               <Route path="/info" element={<Info />} />
@@ -129,31 +129,31 @@ function App() {
               <Route path="/haru" element={<Haru />} />
               <Route path="*" element={<>Not found.</>} />
             </Routes>
-          </BrowserRouter>
-        </div>
+          </div>
 
-        <footer style={{ textAlign: "center", marginTop: "3rem" }}>
-          <p>
-            Built with &hearts; during{" "}
-            <a
-              href="https://hackfs.com/"
-              target="_blank"
-              rel="noopener noreferer"
-            >
-              ETH Global's HackFS hackathon
-            </a>{" "}
-            by{" "}
-            <a
-              href="https://github.com/gitfrosh"
-              target="_blank"
-              rel="noopener noreferer"
-            >
-              Rike
-            </a>
-          </p>
-        </footer>
+          <footer style={{ textAlign: "center", marginTop: "3rem" }}>
+            <p>
+              Built with &hearts; during{" "}
+              <a
+                href="https://hackfs.com/"
+                target="_blank"
+                rel="noopener noreferer"
+              >
+                ETH Global's HackFS hackathon
+              </a>{" "}
+              by{" "}
+              <a
+                href="https://github.com/gitfrosh"
+                target="_blank"
+                rel="noopener noreferer"
+              >
+                Rike
+              </a>
+            </p>
+          </footer>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
